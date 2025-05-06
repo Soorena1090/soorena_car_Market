017bbe60ff83582e49dec640c8b5d15d2ca300e6
Soorena Mahbooby <soorenamahbobi@gmail.com>
2025-05-06 10:19:45 +0330

    nothing

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const axios = require('axios');
 
-// axios.get('https://lm-models.s3.ir-thr-at1.arvanstorage.ir/cars.json')
-// .then(response => {
-//     console.log(response);
-// })
-// .catch (error => {
-//     console.error(error);
-// });
-//-----------------------------------//
-
-// async function marketPriceData() {
-//     const response = await axios.get('https://lm-models.s3.ir-thr-at1.arvanstorage.ir/market_prices.json')
-//     console.log(response);
-// };
-// marketPriceData()
+axios.get('https://lm-models.s3.ir-thr-at1.arvanstorage.ir/cars.json')
+.then(response => {
+    console.log(response);
+})
+.catch (error => {
+    console.error(error);
+});
 
 
-//--------------------------------//
+async function marketPriceData() {
+    const response = await axios.get('https://lm-models.s3.ir-thr-at1.arvanstorage.ir/market_prices.json')
+    console.log(response);
+};
+marketPriceData()
+
 axios.get('https://baha24.com/api/v1/price')
 .then(response => {
     console.log(response);
