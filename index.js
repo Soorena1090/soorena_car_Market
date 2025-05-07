4acd1d09fb3c13cc11fbbebae3f40edd1afe5f3a
Soorena Mahbooby <soorenamahbobi@gmail.com>
2025-05-07 10:51:49 +0330

    write condition and return the result

--- index.js
+++ index.js
@@ -15,3 +15,30 @@ async function fetchCurrency() {
     const response = await axios.get('https://baha24.com/api/v1/price')
     return response.data.USD.Sell;
 };
+
+async function main() {
+    const startTime = Date.now();
+
+    const cars = await fetchCars();
+    const marketData= await marketPriceData();
+    const usdPrice = await fetchCurrency();
+
+    const mapingCars = cars.map(car => {
+        const match = marketData.find(market => {
+            market.barnd === car.brand &&
+            market.model === car.model &&
+            market.year === car.year
+        });
+        const priceDiff = match ? car.price - match.average_price : 0;
+        const mileageDiff = match ? car.mileage - match.average_mileage : 0;
+        const priceUsd = usdPrice ? (car.price / usdPrice) : 0;
+
+        return {
+            ...car,
+            price_diff_from_average: priceDiff,
+            mileage_diff_from_average: mileageDiff,
+            price_usd: Number(priceUsd)
+        };
+    })
+    
+}
