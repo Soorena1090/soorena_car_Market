89b70c65f5da6fbf9319408a4e7ec87b5c9f1304
Soorena Mahbooby <soorenamahbobi@gmail.com>
2025-05-08 09:31:04 +0330

    add function analysis and 3 question

--- index.js
+++ index.js
@@ -17,8 +17,39 @@ async function fetchCurrency() {
     return response.data.USD.sell;
 };
 
+function analysis(cars) {
+    const brandModel = {};
+    cars.forEach(car => {
+        const key = `${car.brand} ${car.model}`;
+        if (brandModel[key]) {
+            brandModel[key]++;
+        }else {
+            brandModel[key] = 1;
+        }
+    });
+    let mostCommon = '';
+    let maxCount = 0;
+    for (const key in brandModel) {
+        if (brandModel[key] > maxCount) {
+            mostCommon = key;
+            maxCount = brandModel[key];
+        }
+    }
+    console.log("Q1: Most common brand & model:", mostCommon, "-", maxCount, "cars")
+    //--------------------------------Q2
+    const top3Expensive = [...cars].sort((a, b) => b.price - a.price).slice(0,3);
+    top3Expensive.forEach(car => {
+        console.log(`${car.brand} ${car.model}, ${car.price} IRR`);
+    });
+
+    //--------------------------------Q3
+    const carByUsd = [...cars].sort((a,b) => a.price_usd - b.price_usd);
+    const min = carByUsd[0].price_usd;
+    const max = carByUsd[carByUsd.length - 1].price_usd;
+    console.log("Q3: USD price difference between most and least expensive cars:", (maxUSD - minUSD), "USD");
+    //---------------------------------Q4
+}
 async function main() {
-    const startTime = Date.now();
 
     const cars = await fetchCars();
     const marketData= await marketPriceData();
